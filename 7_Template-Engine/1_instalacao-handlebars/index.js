93757023460f3898dbbf8ea15bfb74b8e2ef4aab
Daniel Custodio <play.daniel@hotmail.com>
2023-04-16 13:49:06 -0300

    CSS com handlebars

--- 7_Template-Engine/1_instalacao-handlebars/index.js
+++ 7_Template-Engine/1_instalacao-handlebars/index.js
@@ -2,18 +2,49 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 
 const app = express();
-app.engine("handlebars", exphbs.engine());
+const hbs = exphbs.create({
+  partialsDir: ["views/partials"],
+});
+app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+app.use(express.static("public"))
 
 app.get("/posts", (req, res) => {
-  const posts = {
+  const post = {
     title: "Como criar um objeto em JavaScript",
     content: "Neste post, vamos mostrar como criar um objeto em JavaScript.",
     author: "João Silva",
     date: "2023-04-16",
   };
 
-  res.render("blogpost", { posts });
+  res.render("blogpost", { post });
+});
+
+app.get("/blog", (req, res) => {
+  const posts = [
+    {
+      title: "Como criar um objeto em JavaScript",
+      content: "Neste post, vamos mostrar como criar um objeto em JavaScript.",
+      author: "João Silva",
+      date: "2023-04-16",
+    },
+    {
+      title: "Como fazer um loop em um array",
+      content:
+        "Neste post, vamos mostrar como fazer um loop em um array em JavaScript.",
+      author: "Maria Souza",
+      date: "2023-04-15",
+    },
+    {
+      title: "Como usar funções em JavaScript",
+      content: "Neste post, vamos mostrar como usar funções em JavaScript.",
+      author: "Pedro Alves",
+      date: "2023-04-14",
+    },
+    
+  ];
+
+  res.render("blog", {posts});
 });
 
 app.get("/dashboard", (req, res) => {
