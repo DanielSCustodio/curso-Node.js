6ec4e91df76aede3b93e12533a4b92b9bec1e437
Daniel Custodio <play.daniel@hotmail.com>
2023-04-17 13:41:57 -0300

    Inserindo dados no banco

--- 8_MySQL/1_instalacao-mysql/index.js
+++ 8_MySQL/1_instalacao-mysql/index.js
@@ -13,11 +13,34 @@ app.get("/", (req, res)=>{
   res.render("home")
 })
 
+app.use(            //capturar o body
+  express.urlencoded({
+    extended: true
+  })
+)
+
+app.use(express.json())  //capturar o body
+
 const conn = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+});
+
+app.post("/books/insertbook", (req, res)=>{
+  const title = req.body.title
+  const pageqty = req.body.pageqty
+
+  const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}','${pageqty}')`
+  conn.query(sql, (err)=>{
+    if(err){
+      console.log(err);
+    }
+
+    res.redirect("/")
+  })
+
 });
 
 conn.connect((err)=>{
