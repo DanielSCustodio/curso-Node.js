76baccf8bdbe39a5a705f12878a50815b739b43b
Daniel Custodio <play.daniel@hotmail.com>
2023-04-17 16:29:21 -0300

    Resgatando dados com filtro

--- 8_MySQL/1_instalacao-mysql/index.js
+++ 8_MySQL/1_instalacao-mysql/index.js
@@ -1,26 +1,26 @@
-const express = require("express")
-const exphbs = require("express-handlebars")
-const mysql = require("mysql")
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql");
 require("custom-env").env("development.local");
 
-const app = express()
+const app = express();
 
 app.engine("handlebars", exphbs.engine());
-app.set("view engine", "handlebars")
-app.use(express.static("public"))
+app.set("view engine", "handlebars");
+app.use(express.static("public"));
 
-app.get("/", (req, res)=>{
-  res.render("home")
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.use(            //capturar o body
+app.use(
+  //capturar o body
   express.urlencoded({
-    extended: true
+    extended: true,
   })
-)
+);
 
-app.use(express.json())  //capturar o body
-
+app.use(express.json()); //capturar o body
 
 const conn = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -29,40 +29,54 @@ const conn = mysql.createConnection({
   database: process.env.DB_NAME,
 });
 
-app.post("/books/insertbook", (req, res)=>{
-  const title = req.body.title
-  const pageqty = req.body.pageqty
+app.post("/books/insertbook", (req, res) => {
+  const title = req.body.title;
+  const pageqty = req.body.pageqty;
 
-  const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}','${pageqty}')`
-  conn.query(sql, (err)=>{
-    if(err){
+  const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}','${pageqty}')`;
+  conn.query(sql, (err) => {
+    if (err) {
       console.log(err);
     }
-    res.redirect("/books")
-  })
-
+    res.redirect("/books");
+  });
 });
 
-app.get("/books", (req, res)=>{
-  const sql = "SELECT * FROM books"
-
-  conn.query(sql,(err, data)=>{
-    if(err){
+app.get("/books/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = `SELECT * FROM books WHERE id=${id}`;
+  conn.query(sql, (err, data) => {
+    if (err) {
       console.log(err);
-      return
+      return;
     }
 
-    const books = data
-    res.render("books", {books})
-  })
-})
+    const book = data[0];
 
-conn.connect((err)=>{
-  if(err){
+    res.render("book", {book});
+  });
+});
+
+app.get("/books", (req, res) => {
+  const sql = "SELECT * FROM books";
+
+  conn.query(sql, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const books = data;
+    res.render("books", { books });
+  });
+});
+
+conn.connect((err) => {
+  if (err) {
     console.log(err);
   }
   console.log("Aplicação conectada ao banco de dados");
-  app.listen(3000, ()=>{
+  app.listen(3000, () => {
     console.log("Aplicação em execução");
-  })
-})
+  });
+});
