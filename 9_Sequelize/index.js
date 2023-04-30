29d70a4be8f3a15922ec20775516c6d7470f1a18
Daniel Custodio <play.daniel@hotmail.com>
2023-04-29 21:49:39 -0300

    Exclusão de registro relacionado

--- 9_Sequelize/index.js
+++ 9_Sequelize/index.js
@@ -75,6 +75,14 @@ app.post("/address/create", async (req, res) => {
   res.redirect(`/users/edit/${UserId}`);
 });
 
+app.post("/address/delete", async (req, res) => {
+  const { id, UserId } = req.body;
+  await Address.destroy({
+    where: { id: id },
+  });
+  res.redirect(`/users/edit/${UserId}`);
+});
+
 app.get("/", async (req, res) => {
   const users = await User.findAll({ raw: true });
   res.render("home", { users });
