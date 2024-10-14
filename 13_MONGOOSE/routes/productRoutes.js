214d12326911478984e3a0557efb30611d5066ea
Daniel Custódio <play.daniel@hotmail.com>
2024-10-14 15:37:09 -0300

    Mongoose: Inserção de dados.

--- 13_MONGOOSE/routes/productRoutes.js
+++ 13_MONGOOSE/routes/productRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 const ProductController = require("../controllers/ProductController");
 router.get("/create", ProductController.createProduct);
-router.get("/", ProductController.showProducts);
-router.get("/edit/:id", ProductController.editProduct);
-router.get("/:id", ProductController.getProduct);
+/* router.get("/", ProductController.showProducts) */;
+/* router.get("/edit/:id", ProductController.editProduct); */
+/* router.get("/:id", ProductController.getProduct); */
 
 router.post("/create", ProductController.createProductSave);
-router.post("/edit", ProductController.editProductSave);
-router.post("/remove/:id", ProductController.removeProduct);
+/* router.post("/edit", ProductController.editProductSave); */
+/* router.post("/remove/:id", ProductController.removeProduct); */
 
 module.exports = router;
